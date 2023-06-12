569fb96487105fa297a937042fe6a3b7c0c3a71a
Hamdan Salih <hamdansalihpc@gmail.com>
2023-06-12 16:45:27 +0530

    initial

---
+++ question_231_Power_of_two.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 2 or n==1:
+            return True
+        if n % 2 !=0 or n <= 0:
+            return False
+        else:
+            return True and self.isPowerOfTwo(n/2.0)
--- question_283_move_zero.py
+++ question_283_move_zero.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count=0;
+        count=0
         i = 0
         lenght = len(nums)
         while i < lenght:
